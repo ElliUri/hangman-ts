efbfc01e4bc2947bfb9e345f0a706ec5928c082f
Eldana <fineshine6718@gmail.com>
2024-02-01 12:24:05 +0600

    first-commit-TS-Vite

---
+++ src/Keyboard.tsx
@@ -0,0 +1,75 @@
+import s from "./Keyboard.module.css";
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+type KeyboardProps = {
+  disabled?: boolean
+  activeLetters: string[];
+  inactiveLetters: string[];
+  addGuessedLetter: (letter: string) => void;
+};
+// ? means optional variable
+
+function Keyboard({
+  activeLetters,
+  inactiveLetters,
+  disabled = false,
+  addGuessedLetter,
+}: KeyboardProps) {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map((key) => {
+        const isActive = activeLetters.includes(key);
+        const isInactive = activeLetters.includes(key);
+        return (
+          <button
+
+            onClick={() => addGuessedLetter(key)}
+            className={`${s.btn} ${isActive ? s.active : ""} 
+            ${
+              isInactive ? s.inactive : ""
+            }`}
+            key={key}
+            disabled={isInactive || isActive || disabled}
+          >
+            {key}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Keyboard;
